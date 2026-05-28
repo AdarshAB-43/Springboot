5cb4730600faa02e41cb3501a70c5808a32e7304
Adarsh AB <adreshtakudu@gmail.com>
2026-05-28 20:25:09 +0530

    Add files via upload

---
+++ Myapp-5/src/main/java/com/example/demo/Myapp5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myapp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myapp5Application.class, args);
+	}
+
+}
