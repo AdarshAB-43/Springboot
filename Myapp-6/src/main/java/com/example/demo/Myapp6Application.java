e751ef779ca653df2b19ac651bfa7ebaf48c2db3
Adarsh AB <adreshtakudu@gmail.com>
2026-05-28 20:30:55 +0530

    Add files via upload

---
+++ Myapp-6/src/main/java/com/example/demo/Myapp6Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myapp6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myapp6Application.class, args);
+	}
+
+}
