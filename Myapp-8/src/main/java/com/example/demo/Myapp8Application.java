f8b8fc692b73b90c0b8f41438e453461cfb272f3
Adarsh AB <adreshtakudu@gmail.com>
2026-05-28 21:06:27 +0530

    Add files via upload

---
+++ Myapp-8/src/main/java/com/example/demo/Myapp8Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myapp8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myapp8Application.class, args);
+	}
+
+}
